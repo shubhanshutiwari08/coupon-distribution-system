d0e67e5182a2414b5aae3334127484739695c532
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2023-06-23 13:28:49 +0530

    modified

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 from flask import Flask, render_template, request
 import sqlite3
 import pandas as pd
+import random
+import string
 
 app = Flask(__name__)
 
+def generate_coupons(num):
+    coupons = [num]
+    for i in range(num):
+        coupon = (random.choices(string.ascii_uppercase + string.digits, k=4))
+        coupons.append(coupon)
+    return coupons
+
 # Create a function to initialize the database
 def init_db():
-    conn = sqlite3.connect('databasetwo.db')
+    conn = sqlite3.connect('database.db')
     c = conn.cursor()
-    
     c.execute('''CREATE TABLE IF NOT EXISTS form_data
                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
                  name TEXT,
-                 email TEXT)''')
-
-    # c.execute('''CREATE TABLE IF NOT EXISTS form_data
-    #             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #              email TEXT,
-    #              cpfnumber TEXT)''')
-    
-    # c.execute('''CREATE TABLE IF NOT EXISTS coupon_data
-    #             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #              coupon INTEGER)''')
+                 cpfno TEXT,
+                 email TEXT,
+                 num_coupons INTEGER)''')
     conn.commit()
     conn.close()
 
@@ -31,19 +32,26 @@ init_db()
 # Route to handle form submission
 
 # Route to handle form submission
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def main():
+    return render_template("home.html")
+
+
+@app.route('/coupon', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        email = request.form['email']
         name = request.form['name']
-        # cpfnumber = request.form['cpfnumber']
-        # message = request.form['message']
-        # cpfnumber = request.form['cpfnumber']
+        cpfno = request.form['cpfno']
+        email = request.form['email']
+        num_coupons = request.form['num_coupons']
+
+        n = int(num_coupons)
 
         # Read the Excel sheet data
         excel_data = pd.read_excel('data.xlsx')
 
         # Check if the data already exists in the Excel sheet
+        # matching_data = excel_data[(excel_data['cpfno'] == cpfno) & (excel_data['email'] == email)]
         matching_data = excel_data[(excel_data['email'] == email)]
 
         if  matching_data.empty:
@@ -51,7 +59,7 @@ def index():
         else:
             conn = sqlite3.connect('database.db')
             c = conn.cursor()
-            c.execute('SELECT * FROM form_data WHERE name=? AND email=?', (name, email))
+            c.execute('SELECT * FROM form_data WHERE cpfno=? AND email=?', (cpfno, email))
             stored_data = c.fetchone()
             conn.close()
 
@@ -61,20 +69,31 @@ def index():
                 # Add the data to the database
                 conn = sqlite3.connect('database.db')
                 c = conn.cursor()
-                c.execute('INSERT INTO form_data (name, email) VALUES (?, ?)', (name, email))
+                c.execute('INSERT INTO form_data (name, cpfno, email, num_coupons) VALUES (?, ?, ?, ?)', (name, cpfno, email, num_coupons))
 
             conn.commit()
             # conn.close()
 
-            return render_template('success.html')
+            coupons = generate_coupons(n)
+            return render_template('success.html', coupons=coupons,cpfno=cpfno )
+
+
 
     return render_template('index.html')
 
-    
+
+#admin
+
+@app.route('/admin')
+def admin_access():
+    return render_template('admin.html')
+
+
+
     # Route to display the database content
 @app.route('/data')
 def data():
-    conn = sqlite3.connect('databasetwo.db')
+    conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute('SELECT * FROM form_data')
     data = c.fetchall()
@@ -93,10 +112,39 @@ def data():
     return render_template('data.html', data=unique_data, duplicates=duplicates)
 
 
+
+
+# Route to generate and store the number of coupons
+# @app.route('/generate_coupon', methods=['GET','POST'])
+# def generate_coupon():
+#     if request.method == 'POST':
+#         num_coupons = request.form['num_coupons']
+
+#     conn = sqlite3.connect('database.db')
+#     c = conn.cursor()
+
+#     # Get the total number of rows in the form_data table
+#     # c.execute('SELECT COUNT(*) FROM form_data')
+#     # total_rows = c.fetchone()[0]
+
+#     # Check if the total number of rows has reached the limit (1500)
+#     # if total_rows >= 1500:
+#     #     return render_template('coupon_limit.html')
+
+#     # Store the number of coupons in the database
+#     c.execute('INSERT INTO form_data (num_coupons) VALUES (?)', (num_coupons))
+#     conn.commit()
+#     conn.close()
+
+#     return render_template('data.html')
+
+
+
+
 # Route to clear the database
 @app.route('/clear', methods=['GET'])
 def clear_data():
-    conn = sqlite3.connect('databasetwo.db')
+    conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute('DELETE FROM form_data')
     conn.commit()
@@ -112,3 +160,7 @@ def final_coupon():
 # Run the Flask application
 if __name__ == '__main__':
     app.run(port=2020)
+
+
+
+
